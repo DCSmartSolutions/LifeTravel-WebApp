c4a3dacc5bc0c61962f275e1d90989531267cd0b
NicolePT <u201921559@upc.edu.pe>
2023-11-20 22:03:49 -0500

    feat(booking): Modified tour-package component

--- src/app/booking/models/booking.model.ts
+++ src/app/booking/models/booking.model.ts
@@ -3,7 +3,8 @@ import {HourRange} from "../../tour-experience/models/time-picker.model";
 export class Booking {
   id: number = 0;
   tourPackageId: number = 0;
-  touristId: string = '';
+  touristUserId: string = '';
   selectedDate: Date = new Date();
+  tourDate: Date = new Date();
   hourRange: HourRange | undefined = new HourRange() ;
 }
