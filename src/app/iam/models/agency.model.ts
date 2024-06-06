9533d196e3f1d15d280a5a8d00a3287ccacd3266
lucasrucu <lucasruiz1336@gmail.com>
2024-06-05 23:47:18 -0500

    feat: created service and model for agency

---
+++ src/app/iam/models/agency.model.ts
@@ -0,0 +1,10 @@
+export class Agency {
+  legalName: string = 'user';
+  RUC: string = '99999999999';
+  address: string = 'alameda san marcos, chorrillos';
+  agencyPhotoUrl: string =
+    'https://marketplace.canva.com/EAFvvrEdW20/1/0/1600w/canva-blue-and-yellow-illustrative-travel-agency-logo-TWAjs1N3SXo.jpg';
+  description: string = 'agencia turistica en linea';
+  phoneNumber: string = '999999999';
+  webPageUrl: string = 'https://eelslap.com/';
+}
